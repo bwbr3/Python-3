ea6dc7dab2ebbd90a9b092b46ddfb2efe9a08bad
bwbr3 <113187276+bwbr3@users.noreply.github.com>
2022-09-30 21:32:07 -0500

    Add files via upload

---
+++ COmp Lab 5.py
@@ -0,0 +1,74 @@
+# will need index, slicing of strings, iterate over strings/ use multi string methods
+
+import string
+
+def charcter_value(val):
+    charConv = ord(val) - 65
+    return charConv
+    
+if __name__ == "__main__":
+
+    user_input = input("Enter Libary Card. Hit Enter to exit ==> ")
+    output = charcter_value(user_input)
+    print(output)
+
+
+def charcter_value_B(val):
+    charConv = ord(val) - 66
+
+    return charConv
+
+if __name__ == "__main__":
+
+    user_input = input("")
+    output = charcter_value(user_input)
+    print(output)
+
+
+def charcter_value_Z(val):
+    charConv = ord(val) - 90
+
+    return charConv
+
+if __name__ == "__main__":
+
+    user_input = input("")
+    output = charcter_value(user_input)
+    print(output)
+
+    # get check digit 
+
+def get_check_digit_1(val):
+    charConv = 
+  
+    
+get_check_digit
+
+if __name__ == "__main__":
+    
+    user_input = input("")                 
+    output = get_check_digit(user_input)
+    print(output)
+
+ 
+
+def get_school():
+    stuNum = chr()
+    self.asserEqual(stuNum,"School of computing and engineering SCE", "A value of 1 in index 5 should retirn SCE")
+    print(self.asserEqual)
+
+
+
+    
+
+
+      
+
+
+
+
+
+
+    
+
+
